6cdacb34019cc6c4e594f9d6dee2034f93621465
nero winicius <35000458-neron2814@users.noreply.replit.com>
2024-07-09 14:10:46 +0000

    Initial commit

---
+++ index.ts
@@ -0,0 +1,19 @@
+// tipos de variaveis
+const variavel1 //variavel constante
+let variavel2 //variavel mutavel
+var variavel3 //variavel global obs: pouco utilizada
+//conceito de local e global: 
+
+const nome = "joão" // variavel constante nao permite trocar valor
+let nome1 =  "batista" //variavel mutavel permite trocar valor
+
+nome= "pedro"
+nome1= "batista"
+var nome2= "silva"
+ nome = "pedro"
+ nome = "batista"
+ function funcao0 () }
+   const test0= 'test0' // variavel constante local.
+   var test1='test1' // variavel global local.
+
+
